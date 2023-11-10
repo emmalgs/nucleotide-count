2f87ca3ae945a4336d1af0631ec182ca461bb3bc
Emma Gerigscott <elgerig@gmail.com>
2023-11-10 11:56:50 -0800

    Add input validation for nucleotide count function

--- nucleotide-count.js
+++ nucleotide-count.js
@@ -1,14 +1,20 @@
 export function countNucleotides(strand) {
-  const nucleotideCount = {
-    'A': 0,
-    'C': 0,
-    'G': 0,
-    'T': 0
-  }
-  strand.split("").forEach(nucelotide => {
-    nucleotideCount[nucelotide] += 1;
-  });
+  const dna = /^[ATCG]+$/;
 
-  const {A, C, G, T} = nucleotideCount;
-  return `${A} ${C} ${G} ${T}`
+  if (dna.test(strand) || strand.length === 0) {
+    const nucleotideCount = {
+      'A': 0,
+      'C': 0,
+      'G': 0,
+      'T': 0
+    }
+    strand.split("").forEach(nucelotide => {
+      nucleotideCount[nucelotide] += 1;
+    });
+  
+    const {A, C, G, T} = nucleotideCount;
+    return `${A} ${C} ${G} ${T}`
+  } else {
+    throw new Error('Invalid nucleotide in strand')
+  }
 }
